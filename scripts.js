4f8085a8bca5f2fae801906f334aeccdfe2c93c7
jsthewebdev <johnscig3@gmail.com>
2021-11-11 11:51:01 -0500

    Added a UI to my rock paper scissors game, very simple. Honeslty had a harder time than I thought but we push on.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
+const startDiv = document.getElementsByClassName('startScreen')
+const playDiv = document.getElementsByClassName('playScreen')
+const endDiv = document.getElementsByClassName('endScreen')
+const placeHolderText = document.getElementById('resultsPlaceholder')
+
 const selArray = ['ROCK', 'PAPER', 'SCISSORS']
+
+function startPlay() {
+    startDiv[0].classList.add('hide')
+    playDiv[0].classList.remove('hide')
+
+}
 
 function computerPlay() {
     let computerChoice = selArray[Math.floor(Math.random()*selArray.length)];
@@ -10,10 +21,38 @@ let playerPoints = 0;
 let computerPoints = 0;
 let winRound = 'Nothing yet'
 
+function selectItem(elem) {
+    let selBtnElements = document.getElementsByClassName('selBtn');
+    console.log(selBtnElements)
+
+    for (i=0; i < selBtnElements.length; i++) {
+        selBtnElements[i].classList.remove('selected');
+    }
+    elem.classList.add('selected');
+}
+
+function submitChoice() {
+    console.log(playDiv)
+    playDiv[0].classList.add('hide')
+    endDiv[0].classList.remove('hide')
+    game()
+}
+
+function nextRound() {
+    endDiv[0].classList.add('hide')
+    playDiv[0].classList.remove('hide')
+}
+function test() {
+}
+
+
+
+test(); 
 function gameRound() {
     let computerSel = computerPlay();
-    let prePlayerSel = prompt("Rock, paper, or scissors?");
-    let playerSel = prePlayerSel.toUpperCase();
+    let prePlayerSel = document.getElementsByClassName('selected');
+    let stringSel = prePlayerSel[0].innerHTML;
+    let playerSel = stringSel.toUpperCase();
     
     if (playerSel == computerSel) {
         return "It's a tie!"
@@ -48,14 +87,21 @@ function pointCheck() {
     }
 }
 
+let compScoreParagraph = document.getElementById('computerScoreParagraph')
+let playScoreParagraph = document.getElementById('playerScoreParagraph')
+
 function game() {
-    for (let i = 0; i < 4; i++) {
-        console.log(gameRound())
-        console.log("Computer points:" + computerPoints)
-        console.log("Player points:" + playerPoints)
-    }
+    let result = gameRound()
+    console.log(result)
+    placeHolderText.innerHTML = `${result}`;
+    compScoreParagraph.innerHTML = `${computerPoints}`;
+    playScoreParagraph.innerHTML = `${playerPoints}`
+    console.log("Computer points:" + computerPoints)
+    console.log("Player points:" + playerPoints)
     console.log('Game over!')
     console.log(pointCheck())
 }
 
-game();
+
+
+// game();{result}
